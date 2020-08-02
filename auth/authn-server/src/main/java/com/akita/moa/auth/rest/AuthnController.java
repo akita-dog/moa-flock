f489c1274bf9d0d0b2cb1c76730b9a242ce36ce4
hyo <769902400@qq.com>
2020-08-02 23:18:06 +0800

    主体服务

--- auth/authn-server/src/main/java/com/akita/moa/auth/rest/AuthnController.java
+++ auth/authn-server/src/main/java/com/akita/moa/auth/rest/AuthnController.java
@@ -5,6 +5,7 @@ import com.akita.moa.auth.service.AuthnService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class AuthnController {
@@ -12,7 +13,7 @@ public class AuthnController {
     private AuthnService service;
 
     @PostMapping("/login")
-    String login(LoginReq req) {
-        return null;
+    Mono<String> login(LoginReq req) {
+        return service.login(req.getUsername(), req.getPassword());
     }
 }
